800390ecd3c72d232525ceaf6bc49155cec4d073
Lydia Young <lydsjoyy@gmail.com>
2024-05-26 16:25:31 +0000

    add profiles app

---
+++ profiles/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+# Built using code institute django rest framework walkthrough
+class Profile(models.Model):
+    person = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=255, blank=True)
+    about_me = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_qdjgyp')
+    favorite_bird = models.TextField(blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.person}'s profile"
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(person=instance)
+
+post_save.connect(create_profile, sender=User)
