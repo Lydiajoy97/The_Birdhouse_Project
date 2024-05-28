d9bd60717c608ad9c9cac59531efdc0a91e28c22
Lydia Young <lydsjoyy@gmail.com>
2024-05-28 22:07:10 +0000

    fix code and create navbar using bootstrap

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import {Navbar, Container, Nav } from "react-bootstrap";
+import React from 'react'
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 const NavBar = () => {
   return (
-    <NavBar><Container>
-        <NavBar.Brand href="#home">The Birdhouse</NavBar.Brand>
-        <NavBar.Toggle />
-        <NavBar.Collapse className="justify-content-end">
-        <NavBar.Text>
-        Signed in as: <a href="#login"></a>
-        </NavBar.Text>
-        </NavBar.Collapse></Container>
-    </NavBar>
+    <Navbar>
+  <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+  <Navbar.Toggle />
+  <Navbar.Collapse className="justify-content-end">
+    <Navbar.Text>
+      Signed in as: <a href="#login">Mark Otto</a>
+    </Navbar.Text>
+  </Navbar.Collapse>
+</Navbar>
   )
 }
 
