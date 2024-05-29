83c9f68c9c239523807ca0235eaadc6e9e1e881f
Lydia Young <lydsjoyy@gmail.com>
2024-05-29 17:47:17 +0000

    add styles to nav bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <Navbar>
-  <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+    <Navbar className={styles.NavBar}><Container>
+  <Navbar.Brand href="#home">The Birdhouse</Navbar.Brand>
   <Navbar.Toggle />
   <Navbar.Collapse className="justify-content-end">
+    <Nav.Link href= "bird-spot"> Bird Spotting</Nav.Link>
+    <Nav.Link href= "profiles"> Profiles</Nav.Link>
     <Navbar.Text>
       Signed in as: <a href="#login">Mark Otto</a>
     </Navbar.Text>
   </Navbar.Collapse>
-</Navbar>
+</Container></Navbar>
   )
 }
 
