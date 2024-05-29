5e220471ac3bb76d930226cb835cf2b6e8b91e14
Lydia Young <lydsjoyy@gmail.com>
2024-05-29 18:41:42 +0000

    Add css for NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container>home page</Container>
     </div>
   );
 }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,14 +5,12 @@ import styles from '../styles/NavBar.module.css';
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar}><Container>
-  <Navbar.Brand href="#home">The Birdhouse</Navbar.Brand>
-  <Navbar.Toggle />
-  <Navbar.Collapse className="justify-content-end">
-    <Nav.Link href= "bird-spot"> Bird Spotting</Nav.Link>
-    <Nav.Link href= "profiles"> Profiles</Nav.Link>
-    <Navbar.Text>
-      Signed in as: <a href="#login">Mark Otto</a>
-    </Navbar.Text>
+    <Navbar.Brand href="#home">The Birdhouse </Navbar.Brand>
+    <Navbar.Toggle />
+    <Navbar.Collapse className="justify-content-end">
+    <Nav.Link href= "bird-spot"> Bird Spotting <i class="fa-solid fa-feather-pointed"></i></Nav.Link>
+    <Nav.Link href= "signin"> Account <i class="fa-solid fa-dove"></i></Nav.Link>
+    <Nav.Link href= "signup"> Create Account <i class="fa-regular fa-id-card"></i></Nav.Link>
   </Navbar.Collapse>
 </Container></Navbar>
   )
