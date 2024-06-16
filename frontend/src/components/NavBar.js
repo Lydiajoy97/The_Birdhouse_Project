1b12a741b7cdf78ab130d42ef7a6dd6b6cef9911
Lydia Young <lydsjoyy@gmail.com>
2024-06-16 22:59:42 +0000

    create nav bar toggle hook

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -10,11 +10,14 @@ import {
     useSetCurrentUser 
   } from '../contexts/CurrentUserContext';
 import axios from "axios";
+import useClickOutsideToggle from '../hooks/useClickOutsideFile';
 
 /*NavBar built using code insitute walkthrough and bootstrap */
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -87,7 +90,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand> 
@@ -95,7 +98,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)} 
+          aria-controls="basic-navbar-nav" 
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
             <NavLink 
