3699913dc4f0de7caf6e9a9703fb3875cc9c1e39
Lydia Young <lydsjoyy@gmail.com>
2024-06-21 14:04:37 +0000

    change birdpost list api to generics list and add data filters

--- birdpost/views.py
+++ birdpost/views.py
@@ -1,37 +1,32 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from .models import Birdpost
-from rest_framework import serializers
 from birdpost.serializers import BirdpostSerializer
 from the_birdhouse.permissions import IsOwnerOrReadOnly
 
 # Code written from django rest framework walkthrough
-class BirdpostList(APIView):
+class BirdpostList(generics.ListCreateAPIView):
 
     serializer_class = BirdpostSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.annotate(
+        comments_count= Count('comment', distinct=True)
+    ).order_by('-updated_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter, 
+    ]
+    search_fields = [
+        'owner__username',
+        'title',
+    ]
+    ordering_fields = [
+        'comments_count'
     ]
 
-    def get(self, request):
-        birdposts = Birdpost.objects.all()
-        serializer = BirdpostSerializer(birdposts, many=True)
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def post(self, request):
-        serializer = BirdpostSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
 
 class BirdpostDetail(APIView):
     permission_classes = [IsOwnerOrReadOnly]
