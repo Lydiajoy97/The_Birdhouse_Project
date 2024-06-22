669b67642baf4f8717056384adc7d0d3c67ab06e
Lydia Young <lydsjoyy@gmail.com>
2024-06-22 10:49:34 +0000

    add api roots to url paths

--- the_birdhouse/urls.py
+++ the_birdhouse/urls.py
@@ -19,17 +19,16 @@ from .views import root_route, logout_route
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', root_route),
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/logout/', logout_route),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/dj-rest-auth/logout/', logout_route),
+    path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 
-    path('', include('birdpost.urls')),
-    path('', include('profiles.urls')),
-    path('', include('comments.urls')),
+    path('api/', include('birdpost.urls')),
+    path('api/', include('profiles.urls')),
+    path('api/', include('comments.urls')),
 ]
 
 handler404 = TemplateView.as_view(template_name='index.html')
