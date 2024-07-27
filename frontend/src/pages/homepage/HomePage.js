50ddebfea5f8b34d1c26b24646bb54662363bf77
Lydia Young <lydsjoyy@gmail.com>
2024-07-27 18:51:43 +0000

    delete unused text, resize main page image and remove profilepage.js

--- frontend/src/pages/homepage/HomePage.js
+++ frontend/src/pages/homepage/HomePage.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Robin from "../../assets/Robin.jpg";
+import { Container } from 'react-bootstrap';
 
 // From https://cloudinary.com/guides/web-performance/5-ways-to-import-images-in-react-bonus-automation-method
+// Image size from https://reactnative.dev/docs/image-style-props 
 
 const HomePage = () => {
     return (
-      <div>
-        <img  src={Robin} alt="Robin in a tree" />
-      </div>
+      <Container>
+        <img
+            style={{ 
+              resizeMode: 'cover',
+              height: 900,
+              width: 1100,
+            }} className='robin-img' src={Robin} alt="Robin in a tree" />
+      </Container>
     );
   };
 
