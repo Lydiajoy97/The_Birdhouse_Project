5b015257e33a2bb52587f3f47071b9210fd24b9f
Lydia Young <lydsjoyy@gmail.com>
2024-09-28 20:27:50 +0000

    remove slash from birdpost url path

--- birdpost/urls.py
+++ birdpost/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from birdpost import views
 
 urlpatterns = [
-    path('birdpost/', views.BirdpostList.as_view()),
+    path('birdpost', views.BirdpostList.as_view()),
     path('birdpost/<int:pk>', views.BirdpostDetail.as_view()),
 ]
