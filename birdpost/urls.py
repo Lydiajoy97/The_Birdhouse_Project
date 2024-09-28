c6cee1c705ec57c5d2ac5ab647bcf51483c660e6
Lydia Young <lydsjoyy@gmail.com>
2024-09-28 20:21:26 +0000

    add commas to url paths on comments and birdpost

--- birdpost/urls.py
+++ birdpost/urls.py
@@ -3,5 +3,5 @@ from birdpost import views
 
 urlpatterns = [
     path('birdpost/', views.BirdpostList.as_view()),
-    path('birdpost/<int:pk>', views.BirdpostDetail.as_view())
+    path('birdpost/<int:pk>', views.BirdpostDetail.as_view()),
 ]
