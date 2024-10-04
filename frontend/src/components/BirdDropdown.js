40f1969b474ff4bbb035bb1d175f83b0c3d3b45d
Lydia Young <lydsjoyy@gmail.com>
2024-10-04 16:24:51 +0000

    fix bird drop down feature on the birddropdown page so that a user can seet a catorgory of bird

--- frontend/src/components/BirdDropdown.js
+++ frontend/src/components/BirdDropdown.js
@@ -1,9 +1,8 @@
 import React, {useState} from 'react';
 // https://dev.to/antdp425/populate-dropdown-options-in-react-1nk0
 function BirdDropdown() {
-  const [setBird] = useState({});
 
-  let Type_of_bird = [
+  let BIRD_CHOICES = [
     { label: "Owl", value: "owl" },
     { label: "Eagle", value: "Eagle" },
     { label: "Heron", value: "Heron" },
@@ -12,13 +11,17 @@ function BirdDropdown() {
     { label: "unknown", value: "unknown" }
   ]
 
+  let [bird, setBird] = useState("Type of bird")
+
   let handleBirdChange = (e) => {setBird(e.target.value)}
   return (
     <div className="ml-auto">
+      <option>What type of bird is it?</option>
       <br />
       <select onChange={handleBirdChange}> 
-        <option> Type of Bird </option>
-        {Type_of_bird.map((bird) => <option key={bird.label} value={bird.value}>{bird.label} </option>)}
+        <option>{bird}</option>
+        {BIRD_CHOICES.map((bird) => <option key={bird.label} value={bird.value}>{bird.label} </option>)}
+        
       </select>
     </div>
   );
