be8091bfe93da32d94c2745b440d7dcbf4f170fa
Lydia Young <lydsjoyy@gmail.com>
2024-10-19 19:52:51 +0000

    add approval line for birdposts so that the admin has to approve posts

--- birdpost/models.py
+++ birdpost/models.py
@@ -23,3 +23,4 @@ class Birdpost(models.Model):
     Type_of_bird = models.CharField(max_length=60, choices=BIRD_CHOICES, default='unknown')
     updated_at = models.DateTimeField(auto_now=True)
     uploaded_by = models.CharField(max_length=255, default='name')
+    approved = models.BooleanField(default=False)
